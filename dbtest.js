fb9e9148a761d058c03a989b365667e3397de469
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-09-18 14:48:08 +0300

    Naming sever and creating collection

--- dbtest.js
+++ dbtest.js
@@ -1,14 +1,23 @@
 import { MongoClient } from "mongodb";
 import { dbConfig } from "./db-config.js";
 
-const { url } = dbConfig;
+const { url, dbName } = dbConfig;
 
 const client = new MongoClient(url)
+
+let personDocument = {
+    name: { first: 'Alan', last: 'Turing' },
+    birth: new Date(1912, 5, 23),
+    death: new Date(1954, 5, 7),
+    contribs: ['Turing machine', 'Turing test', 'Turingery']
+};
 
 const app = async () => {
     try {
         await client.connect()
         console.log('Connected to server')
+        const db = client.db(dbName)
+        const col = db.collection('people')
     } catch (error) {
         console.log(error.stack)
     }
